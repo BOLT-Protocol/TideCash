c7e2c228ba0b2e7ec218b2cba0755873e4e7e6c7
Luphia Chang <luphia@mermer.cc>
2021-02-02 09:07:19 +0800

    Add XPASwap contract

---
+++ contracts/library/SafeMath.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: unlicensed
+// a library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
+pragma solidity ^0.7.4;
+
+library SafeMath {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, 'ds-math-add-overflow');
+    }
+
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, 'ds-math-sub-underflow');
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
+    }
+}
